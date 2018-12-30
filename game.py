4e776020b24afa41e90fcad82764d8ec449faccc
jrs322 <jrs322@case.edu>
2018-12-30 13:30:01 -0500

    First Commit

---
+++ game.py
@@ -0,0 +1,35 @@
+import pygame
+from pygame.locals import *
+
+class GOL:
+    def __init__(self):
+        self.running = True
+        self._display_surf_ = None
+        self.size = self.weight, self.height = 800, 450
+    def on_init(self):
+        pygame.init()
+        self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
+        self._running = True
+    def on_event(self, event):
+        if event.type == pygame.QUIT:
+            self._running = False
+    def on_loop(self):
+        pass
+    def on_render(self):
+        pass
+    def on_cleanup(self):
+        pygame.quit()
+    def on_execute(self):
+        if self.on_init() == False:
+            self._running = False
+
+        while( self._running ):
+            for event in pygame.event.get():
+                self.on_event(event)
+            self.on_loop()
+            self.on_render()
+        self.on_cleanup()
+
+if __name__ == "__main__" :
+    game_of_life = GOL()
+    game_of_life.on_execute()
